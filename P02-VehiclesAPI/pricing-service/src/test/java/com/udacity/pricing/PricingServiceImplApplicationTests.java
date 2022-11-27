d86569c782e4a3514759879525eb69deca55d308
Dennis Gijzen <dennisgijzen@gmail.com>
2022-11-27 13:49:23 +0100

    pricing service as micro service, registered, and service folder deleted

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceImplApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceImplApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PricingServiceApplicationTests {
+public class PricingServiceImplApplicationTests {
 
 	@Test
 	public void contextLoads() {
